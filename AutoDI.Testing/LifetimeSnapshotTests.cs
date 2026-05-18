b6cde3aec5bc66eabd98ca41434242b5451fed88
Nemanja Kajzer <kajzernemanja@gmail.com>
2026-05-18 16:12:40 +0200

    P4: Implement emitting logic and write unit tests for all lifetimes

---
+++ AutoDI.Testing/LifetimeSnapshotTests.cs
@@ -0,0 +1,84 @@
+﻿using AutoDI.SourceGenerator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Reflection;
+
+namespace AutoDI.Testing
+{
+    public class LifetimeSnapshotTests
+    {
+        private static Task VerifyGenerator(string source)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+            var references = new[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(Assembly.Load("System.Runtime").Location),
+                MetadataReference.CreateFromFile(Assembly.Load("netstandard").Location),
+                MetadataReference.CreateFromFile(typeof(RegisterScopedAttribute).Assembly.Location)
+            };
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName: "TestAssembly",
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            var generator = new AutoDIGenerator();
+
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(
+                generators: new[] { generator.AsSourceGenerator() }
+            );
+
+            driver = driver.RunGenerators(compilation);
+
+            return Verifier.Verify(driver);
+        }
+
+        [Fact]
+        public Task Scoped_SingleInterface_EmitsAddScoped()
+        {
+            var source = @"
+                using AutoDI;
+                namespace TestApp
+                {
+                    public interface IFooService {}
+                    [RegisterScoped]
+                    public class FooService : IFooService {}
+                }";
+
+            return VerifyGenerator(source);
+        }
+
+        [Fact]
+        public Task Singleton_SingleInterface_EmitsAddSingleton()
+        {
+            var source = @"
+                using AutoDI;
+                namespace TestApp
+                {
+                    public interface IFooService {}
+                    [RegisterSingleton]
+                    public class FooService : IFooService {}
+                }";
+
+            return VerifyGenerator(source);
+        }
+
+        [Fact]
+        public Task Transient_SingleInterface_EmitsAddTransient()
+        {
+            var source = @"
+                using AutoDI;
+                namespace TestApp
+                {
+                    public interface IFooService {}
+                    [RegisterTransient]
+                    public class FooService : IFooService {}
+                }";
+
+            return VerifyGenerator(source);
+        }
+    }
+}
---
+++ AutoDI.Testing/ModuleInitializer.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AutoDI.Testing
+{
+    public static class ModuleInitializer
+    {
+        [ModuleInitializer]
+        public static void Init() => VerifySourceGenerators.Initialize();
+    }
+}
